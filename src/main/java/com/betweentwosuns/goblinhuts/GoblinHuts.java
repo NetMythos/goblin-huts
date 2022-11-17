89b09564f393e14c945f83fd573edac6cbaac5ab
Lincoln <117424099+codelincoln@users.noreply.github.com>
2022-11-16 18:31:34 -0800

    fix: fix geckolib default models being loaded on server
    also shrink goblin bannerette item form

--- src/main/java/com/betweentwosuns/goblinhuts/GoblinHuts.java
+++ src/main/java/com/betweentwosuns/goblinhuts/GoblinHuts.java
@@ -4,7 +4,6 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.minecraft.block.entity.BlockEntityType;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
