e9f52b64283a15e3d5bb0f040da4d0cfd626938c
Lincoln <117424099+codelincoln@users.noreply.github.com>
2022-11-18 00:52:03 -0800

    feat: Copy trident behaviors to goblin spear

--- src/main/java/com/betweentwosuns/goblinhuts/GoblinHuts.java
+++ src/main/java/com/betweentwosuns/goblinhuts/GoblinHuts.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -12,21 +13,24 @@ import software.bernie.geckolib3.GeckoLib;
 import com.betweentwosuns.goblinhuts.block.GoblinBanneretteBlock;
 import com.betweentwosuns.goblinhuts.block.tile.GoblinBanneretteTileEntity;
 import com.betweentwosuns.goblinhuts.items.GoblinBanneretteBlockItem;
+import com.betweentwosuns.goblinhuts.items.GoblinSpear;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class GoblinHuts implements ModInitializer {
 
+	public static final String MOD_ID = "goblinhuts";
+
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("goblinhuts");
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	public static final GoblinBanneretteBlock GOBLIN_BANNERETTE = new GoblinBanneretteBlock();
 	public static final BlockEntityType<GoblinBanneretteTileEntity> GOBLIN_BANNERETTE_ENTITY = Registry.register(
 			Registry.BLOCK_ENTITY_TYPE,
-			new Identifier("goblinhuts", "goblin_bannerette_entity"),
+			new Identifier(MOD_ID, "goblin_bannerette_entity"),
 			FabricBlockEntityTypeBuilder.create(GoblinBanneretteTileEntity::new, GOBLIN_BANNERETTE).build()
 			);
 	public static final GoblinBanneretteBlockItem GOBLIN_BANNERETTE_BLOCK_ITEM = new GoblinBanneretteBlockItem(GOBLIN_BANNERETTE, new FabricItemSettings().group(ItemGroup.MISC));
@@ -41,7 +45,9 @@ public class GoblinHuts implements ModInitializer {
 
 		GeckoLib.initialize();
 
-		Registry.register(Registry.BLOCK, new Identifier("goblinhuts", "goblin_bannerette"), GOBLIN_BANNERETTE);
-		Registry.register(Registry.ITEM, new Identifier("goblinhuts", "goblin_bannerette"), GOBLIN_BANNERETTE_BLOCK_ITEM);
+		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "goblin_bannerette"), GOBLIN_BANNERETTE);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "goblin_bannerette"), GOBLIN_BANNERETTE_BLOCK_ITEM);
+
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "goblin_spear"), GoblinSpear.GOBLIN_SPEAR);
 	}
 }
